dbfa1964653a97886634df543ab5ce1a865e4d10
Coy Sanders <coymeetsworld@gmail.com>
2016-10-20 03:07:18 -0700

    DataVis: Geopath: Able to draw circles of meteorites and put them in proper location on canvas.

--- js/geopath.js
+++ js/geopath.js
@@ -7,12 +7,33 @@ $(document).ready(function() {
     .attr("id", "mapContainer")
     .attr("width", width)
     .attr("height", height);
-
-
+    
+  var x = d3.scaleLinear().range([0, width]).domain([-180, 180]);
+  var y = d3.scaleLinear().range([height, 0]).domain([-90, 90]);
+   
+  function getCoordinates(geoObj, coordinate) {
+    
+    if (!geoObj) { return 0; }
+    
+    if (coordinate === 'long') { return geoObj.coordinates[0]; }
+    else if (coordinate === 'lat') { return geoObj.coordinates[1]; }
+    
+    return -1;
+    
+  }
+  
   d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function(error, data) {
     if (error) throw error;
   
-    console.log(data);
+    console.log(data.features);
+    
+    svg.selectAll("circle")
+       .data(data.features)
+       .enter().append("circle")
+       .attr("cx", function(d) { return x(getCoordinates(d.geometry, 'long')); })
+       .attr("cy", function(d) { return y(getCoordinates(d.geometry, 'lat')); })        
+       .attr("r", 10);
+      
   });
 
 
