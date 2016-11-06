c4b87c37f0f3e00988e7597786a26657c6d4c804
Coy Sanders <coymeetsworld@gmail.com>
2016-11-06 01:22:52 -0800

    DataVis: Geopath: Refactoring, cleaning up code.

--- js/geopath.js
+++ js/geopath.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
   
-
 			var width=window.innerWidth;
 			var height=window.innerHeight;
-			console.log("width: " + width);
-			console.log("height: " + height);
-			var defaultScale = 1;
 									
 			var svg = d3.select("svg");
 			
@@ -14,15 +10,15 @@ $(document).ready(function() {
     							 .translateExtent([width/2, height/2])
     							 .on("zoom", zoomed);
 									
-			var view = svg.append('rect')
-					.attr('x', 0)
-					.attr('y', 0)
-  				.attr('width', width)
-  				.attr('height', height)
-  				.attr('fill', '#266D98');
+			var ocean = svg.append('rect')
+										 .attr('x', 0)
+										 .attr('y', 0)
+  									 .attr('width', width)
+  									 .attr('height', height)
+  									 .attr('fill', '#266D98');
 					
 			var map = svg.append( "g" ).attr( "id", "map" )
-										.attr("transform", "translate(" + 0 + "," + 0 + ") scale(" + defaultScale + ")");
+										.attr("transform", "translate(" + 0 + "," + 0 + ")");
 										
 			var projection = d3.geoMercator()
 												 .scale(250)	
@@ -30,25 +26,23 @@ $(document).ready(function() {
 			var geoPath = d3.geoPath()
     									.projection( projection );
 											
-			
-			//d3.json("js/countries.json", function(error, data) {
 			d3.json("https://dl.dropboxusercontent.com/u/26748984/web-project-resources/freecodecamp/D3-GeoPath/countries.json", function(error, data) {
-    		console.log(data);
-    		map.selectAll( "path" )
-    		.data(data.features )
-    		.enter()
-    		.append( "path" )
-    		.attr( "d", geoPath );
+    		map.selectAll("path")
+    			 .data(data.features)
+    			 .enter()
+    			 .append("path")
+    			 .attr("d", geoPath);
+					 
 				svg.call(zoom);
   		});
 			
-			var meteorites = svg.append( "g" ).attr( "id", "meteorites" )
-													.attr("transform", "translate(" + 0 + "," + 0 + ") scale(" + defaultScale + ")");
+			var meteorites = svg.append( "g" )
+													.attr( "id", "meteorites" )
+													.attr("transform", "translate(" + 0 + "," + 0 + ")");
 													
 			d3.json('https://dl.dropboxusercontent.com/u/26748984/web-project-resources/freecodecamp/D3-GeoPath/meteorite-strike-data.json', function(error, data) {
     		if (error) throw error;
   
-    		console.log(data.features);
 				var meteorSizes = data.features.reduce(function(prev, curr) {
                             if (curr.properties.mass) {
                               prev.push(+curr.properties.mass);
@@ -57,14 +51,13 @@ $(document).ready(function() {
                            }, []);
 
 
-    		var meteorRange = d3.scalePow().range([2, 100]).domain(d3.extent(meteorSizes)).exponent(0.7);
+    		var meteorRange = d3.scalePow().range([3, 50]).domain(d3.extent(meteorSizes)).exponent(0.4);
     
-
     		meteorites.selectAll("circle")
        						.data(data.features)
       		 				.enter().append("circle")
-       						.attr("cx", function(d) { return projection([d.properties.reclong,d.properties.reclat])[0]; })
-       						.attr("cy", function(d) { return projection([d.properties.reclong,d.properties.reclat])[1]; })        
+       						.attr("cx", function(d) { return getCoordinates(projection, d.properties, 'long'); })
+       						.attr("cy", function(d) { return getCoordinates(projection, d.properties, 'lat'); })        
        						.attr("r", function(d) { return meteorRange(+d.properties.mass) })
        						.attr("d", geoPath)
       		 				.attr("class", function(d) { return getRandomColorClass(); })
@@ -73,12 +66,10 @@ $(document).ready(function() {
 									})
 									.on("mouseover", function() {
 										var meteoriteData = d3.select(this).datum().properties;
-										//console.log(meteoriteData);	
 										
 										var xPos = parseFloat(d3.select(this).attr("cx"));
 										var yPos = parseFloat(d3.select(this).attr("cy"));
 										var radius = parseFloat(d3.select(this).attr("r"));
-										//console.log(xPos,yPos,radius);
 										
 										var xRect, yRect;	
 										if (xPos+radius+250 > width) { xRect = xPos-radius-250;	}
@@ -116,15 +107,15 @@ $(document).ready(function() {
     
  			});
 				
-
+				
 			function zoomed() {
 				var translate = "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ")";
-				//var scale = "scale(" + (d3.event.transform.k*width/1900) + ")";
-				var scale = "scale(" + (defaultScale*d3.event.transform.k) + ")";
+				var scale = "scale(" + (d3.event.transform.k) + ")";
   			map.attr("transform", translate + " " + scale);
-				scale = "scale(" + (defaultScale*d3.event.transform.k) + ")";
+				scale = "scale(" + (d3.event.transform.k) + ")";
   			meteorites.attr("transform", translate + " " + scale);
 			}
+			
 			
 			function getRandomColorClass() {
     		/* 10 colors to choose from. */ 
@@ -132,14 +123,12 @@ $(document).ready(function() {
     		return 'gradient' + rand;
   		}
 
-			function getCoordinates(geoObj, coordinate) {
-    
-    		if (!geoObj) { return 0; }
-    
-    		if (coordinate === 'long') { return geoObj.coordinates[0]; }
-    		else if (coordinate === 'lat') { return geoObj.coordinates[1]; }
-    
-    		return -1;
+
+			function getCoordinates(projection, geoObj, coordinate) {
+				var point = projection([geoObj.reclong, geoObj.reclat]);
+    		if (coordinate === 'long') { return point[0]; }
+    		else if (coordinate === 'lat') { return point[1]; }
+				return -1; //Shouldn't get here
 		  }
 
 });
